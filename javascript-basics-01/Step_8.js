0b4bb61f5223fcfde07afe5be473ace4d1309fe0
Rayan <rayan.elchakik@gmail.com>
2021-11-24 18:25:23 +0200

    Step 8 of 01 Completed

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,9 @@
+function calculate() {
+    var ageValue = document.getElementById('age').value;
+    if (ageValue >= 18 ){
+        alert ('You are over 18!');
+    }
+    else {
+        alert ('You are bellow 18!');
+    }
+}
