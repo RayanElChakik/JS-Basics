0d18e70ebc23112b177585b916fb87313b5ce6dc
Rayan <rayan.elchakik@gmail.com>
2021-11-24 18:08:45 +0200

    Step 4 of 01 Completed

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,8 @@
+function calculate() {
+    var theName = document.getElementById('name').value;
+    var theSurname = document.getElementById('surname').value;
+    var theCity = document.getElementById('city').value;
+    
+    
+     alert("Name : " + theName + "\n" + "Surname : " + theSurname + "\n" +  "City : " + theCity );
+}
