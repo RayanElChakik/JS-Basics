91210bc19a3c787641d7495eb94396397656e525
Rayan <rayan.elchakik@gmail.com>
2021-11-24 18:12:12 +0200

    Step 5 of 01 Completed

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,7 @@
+function calculate() {
+    var fNb = document.getElementById('first_number').value;
+    var sNb = document.getElementById('second_number').value;
+    var result = fNb * sNb;
+    
+     alert(" Multiplication result of both numbers is: " + result);
+}
