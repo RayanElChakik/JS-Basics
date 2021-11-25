f33417e0fc277426e92073f9092d00ab106889c6
Rayan <rayan.elchakik@gmail.com>
2021-11-25 09:00:43 +0200

    Step 3 of 03 Completed

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,9 @@
+function greenText(){
+    document.getElementById("text").style.color = "green";
+}
+function redText(){
+    document.getElementById("text").style.color = "red";
+}
+function blueText(){
+    document.getElementById("text").style.color = "blue";
+}
