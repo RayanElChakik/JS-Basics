d6217d47255faa383981263981bb723ce22db3e5
Rayan <rayan.elchakik@gmail.com>
2021-11-25 07:44:44 +0200

    Step 1 0f 03 Completed

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,6 @@
+function styleImage(){
+        document.getElementById("image1").style.border = "3px solid red";
+}
+function normalImg(){
+    document.getElementById("image1").style.border = "none";
+}
